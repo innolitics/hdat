d16422ae4a359bf59fdc1704132b984bda6389b9
J. David Giese <johndgiese@gmail.com>
2018-01-25 01:46:22 -0500

    Update hdat so that you select previous result_ids

--- resultspec.py
+++ resultspec.py
@@ -19,16 +19,36 @@ def resolve_resultspec(archive, resultspec):
     else:
         resultspec_parts = resultspec.split('/')
 
-    if len(resultspec_parts) < 3:
+    pick_latest = len(resultspec_parts) == 2
+    pick_by_index = len(resultspec_parts) == 3 and resultspec_parts[2].startswith('~')
+    pick_by_result_id = len(resultspec_parts) == 3 and not pick_by_index
+
+    if pick_latest or pick_by_index:
+        if pick_latest:
+            i = -1
+        else:
+            try:
+                i = -1 - int(resultspec_parts[2][1:])
+            except ValueError:
+                msg = 'Invalid resultspec "{}"; the third part must be a valid result_id' + \
+                        'or a tilde followed by an integer, but not "{}"'
+                raise AbortError(msg.format(resultspec, resultspec_parts[2][1]))
+
+
         # TODO: make this faster in the event there are many results
-        results = archive.select_all(*resultspec_parts)
+        results = archive.select_all(*resultspec_parts[:2])
         if len(results) == 0:
             msg = 'Unable to locate any results matching "{}" in the archive at "{}"'
             raise AbortError(msg.format(resultspec, archive.root))
         else:
-            return results[-1]
+            try:
+                print(i)
+                return results[i]
+            except IndexError:
+                msg = 'Unable to locate any results matching "{}", there are only {} results present.'
+                raise AbortError(msg.format(resultspec, i))
 
-    elif len(resultspec_parts) == 3:
+    elif pick_by_result_id:
         result = archive.select(*resultspec_parts)
         if result is None:
             msg = 'Unable to locate result "{}" in the archive at "{}"'
