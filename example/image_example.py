0b9941e207e8e388ab05bd6b8763b7743eba33ee
Dillon Williams <dillonwilliams@fastmail.com>
2018-02-23 18:25:00 -0600

    Add end-to-end example with image flip and blur
    Images are public domain from the USDA ARS image gallery at https://www.ars.usda.gov/oc/images/image-gallery/

---
+++ example/image_example.py
@@ -0,0 +1,25 @@
+import numpy as np
+import imageio
+from scipy import ndimage
+import os
+
+
+def rotate_and_blur(image_path):
+    full_path = os.path.join(os.path.dirname(__file__), image_path)
+    img_array = imageio.imread(full_path)
+
+    '''
+    this can be replaced with `np.flipud(img_array)` or a related
+    rotation method; when running `hdat run` the results
+    and metrics should match within the defined examplesuite.check method
+    '''
+    flipped_array = np.flip(img_array, 0)
+
+    '''
+    changing the filter parameter `sigma` will alter the resulting image,
+    failing the ExampleSuite.check method. `hdat show` will display
+    the output, and then `hdat verify` can confirm the resulting
+    image as the golden result for future runs
+    '''
+    new_img_array = ndimage.gaussian_filter(flipped_array, sigma=(3, 3, 1))
+    return new_img_array
