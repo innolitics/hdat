e1e9d0496cb3c2267e045ea5052c3f5cca26eb73
Russell <txrjk96@utexas.edu>
2018-07-12 20:41:25 -0500

    Refine csv subcommand handler

--- hdat/hdat_cli.py
+++ hdat/hdat_cli.py
@@ -47,6 +47,8 @@ def parse_arguments(arguments):
     csv_parser = subparsers.add_parser('csv', help=csv_help)
     csv_result_help = 'results to be printed'
     csv_parser.add_argument('resultspec', nargs='?', default='', metavar='<result>', help=csv_result_help)
+    csv_keys_help = 'keys specifier to print data'
+    csv_parser.add_argument('--keys', nargs='?', dest='keys', help=csv_keys_help)
 
     return parser.parse_args(arguments)
 
@@ -99,8 +101,7 @@ def hdat_cli(arguments, suites, golden_store, archive, git_info):
     elif args.command == 'csv':
         results = resolve_resultspecs(archive, args.resultspec)
         for result in results:
-            print_result(result)
-
+            print_result(result, args.keys)
 
 
 def show_result(suites, result):
@@ -130,42 +131,60 @@ def diff_results(suites, golden_result, result):
         raise AbortError(msg.format(print_resultspec(result), e))
 
 
-def print_result(result):
+def get_result_data(result, input_key_list):
     keys = []
     data = []
 
     for key, value in result.items():
-        # print(key, value)
         if isinstance(value, dict):
             for nested_key, nested_value in value.items():
-                # print("inside:", nested_key, nested_value)
                 joined_key = ".".join([key, nested_key])
-                keys.append(joined_key)
-                data.append(nested_value)
-                # print("appended {} to keys and {} to data".format(joined_key, nested_value))
+                if "".join([key, ".*"]) in input_key_list:
+                    keys.append(joined_key)
+                    data.append(nested_value)
+                elif joined_key in input_key_list:
+                    keys.append(joined_key)
+                    data.append(nested_value)
         else:
-            if key != "context":
+            if "context" not in key and key in input_key_list:
                 keys.append(key)
                 data.append(value)
-                # print("appended {} to keys and {} to data".format(key, value))
 
-    data_str = [str(value) for value in data]
+    return keys, data
 
-    keys_out = ", ".join(keys)
+
+def get_wrong_keys(input_key_list, result_keys):
+    wrong_keys = []
+
+    for in_key in input_key_list:
+        if in_key not in result_keys:
+            matched_joined_keys = [key for key in result_keys if in_key.replace("*", "") in key]
+            if ".*" not in in_key or not matched_joined_keys:
+                wrong_keys.append(in_key)
+
+    return ", ".join(wrong_keys)
+
+
+def print_result(result, input_keys_str):
+    if not input_keys_str:
+        input_keys_str = 'case_id,result_id,run_on,gith_hash,metrics.*'
+    
+    input_key_list = input_keys_str.split(",")
+
+    result_keys, result_data = get_result_data(result, input_key_list)
+
+    wrong_keys = get_wrong_keys(input_key_list, result_keys)
+
+    data_str = [str(value) for value in result_data]
+    keys_out = ", ".join(result_keys)
     data_out = ", ".join(data_str)
 
-    '''
-    print("Keys: ", keys)
-    print("Values: ", data)
-    input("Keys and values printed for case: {} in suite: {}. Press any button to continue.\n".format(result["case_id"], result["suite_id"]))
-    print(keys_out)
-    print(data_out)
-    input("Formatted output printed for case: {} in suite: {}. Press any button to continue.\n".format(result["case_id"], result["suite_id"]))
-    '''
+    if keys_out:
+        print("Matched data in suite {} case {}.".format(result["suite_id"], result["case_id"]))
+        print(keys_out)
+        print(data_out)
+    else:
+        print("No keys matched data in suite {} case {}.".format(result["suite_id"], result["case_id"]))
 
-    with open('results.csv', 'a') as f:
-        f.write("{}\n".format(keys_out))
-        f.write("{}\n".format(data_out))
-    input("Output printed to results.csv. Press any button to continue.\n")
-
-    # have option of specifying --keys by checking if key/joined_key exists in arguments given
+    if wrong_keys:
+        print("Keys not found: {}".format(wrong_keys))
