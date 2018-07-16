62c74623bc232599bbfda1e76b1ecb0b21255a0e
Russell <txrjk96@utexas.edu>
2018-07-16 11:35:27 -0500

    Add nested key test

--- tests/csv_test.py
+++ tests/csv_test.py
@@ -2,19 +2,20 @@ import hdat.hdat_cli as hdat
 
 
 class TestGetKeys:
+
     def test_keys_default(self, mock_results, mock_keys):
         assert hdat.get_result_keys(mock_results[0], mock_keys) == (
             ['case_id', 'ran_on', 'result_id']
         )
 
-    def test_specific_metrics(self, mock_results):
+    def test_non_existing_keys(self, mock_results):
         in_keys = ['case_id', 'result_id', 'metrics.mean', 'metrics.std']
 
         assert hdat.get_result_keys(mock_results[0], in_keys) == (
             ['case_id', 'result_id']
         )
 
-    def test_with_metrics(self):
+    def test_with_wildcard(self):
         mock_result = {
             "case_id": "cid",
             "commit": "c1",
@@ -36,6 +37,28 @@ class TestGetKeys:
             'case_id', 'metrics.max', 'metrics.mean',
             'metrics.min', 'metrics.size', 'metrics.std',
             'ran_on', 'result_id', 'status'
+        ]
+
+    def test_nested_keys(self):
+        mock_result = {
+            "case_id": "cid",
+            "commit": "c1",
+            "metrics": {
+                "max": 100,
+                "mean": 50,
+                "min": 1,
+                "size": 100,
+                "std": 3
+            },
+            "ran_on": 123.456,
+            "result_id": "r1",
+            "status": "pass",
+            "suite_id": "mock_suite"
+        }
+        in_keys = ['case_id', 'metrics.max', 'metrics.mean', 'metrics.size']
+
+        assert hdat.get_result_keys(mock_result, in_keys) == [
+            'case_id', 'metrics.max', 'metrics.mean', 'metrics.size'
         ]
 
     def test_dot_modifier(self):
